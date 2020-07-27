219199dbe2b3546c305357fee19ed50ea4bb787f
WiseMrMusa <37452594+WiseMrMusa@users.noreply.github.com>
2020-07-27 16:26:16 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-const download = document.getElementById('generate-dp');
-
 function getInputValue()
 {
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');    
     var inputVal = document.getElementById('Mname').value;
 
     canvas.width = 1080;
     canvas.height = 1080;
-
-    const centreX = canvas.width/2;
-    const centreY = canvas.height/2;
 
     const frame = document.getElementById('frame');
     ctx.drawImage(frame,0,0);
@@ -19,12 +14,12 @@ function getInputValue()
     ctx.fillText(`${inputVal}`,frame.width/2,800);
 
     if(window.navigator.msSaveBlob){
-        window.navigator.msSaveBlob(canvas.msToBlob(),'OFI_Eid.png');
+        window.navigator.msSaveBlob(canvas.msToBlob(),`OFI_Eid_${inputVal}.png`);
     } else {
         const a = document.createElement('a');
         document.body.appendChild(a);
         a.href = canvas.toDataURL();
-        a.download = 'OFI_Eid_${inputVal}.png';
+        a.download = `OFI_Eid_${inputVal}.png`;
         a.click();
         document.body.removeChild(a);
     }
