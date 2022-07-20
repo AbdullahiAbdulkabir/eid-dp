eb1c0ad1e8767faa71556a17730afce831b7c966
Abdullahi Abdulkabir <33360580+AbdullahiAbdulkabir@users.noreply.github.com>
2022-07-20 04:29:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function getInputValue() {
     ctx.textAlign = 'center';
     ctx.font = 'bold 63px Abhaya Libre';
     ctx.fillStyle = "#ffffff";
-    ctx.fillText(`${inputVal}`, frame.width / 2, 910);
+    ctx.fillText(`${inputVal}`, frame.width / 2, 1000);
 
     if (window.navigator.msSaveBlob) {
         window.navigator.msSaveBlob(canvas.msToBlob(), `DW_${inputVal}.png`);
